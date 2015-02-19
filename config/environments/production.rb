77be0472b93ee7173df9de390947871e894a6a81
Alzater <alzater@mail.ru>
2015-02-19 19:16:59 +0300

    passwords del

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => '',
-    :user_name            => "alzatermail",
-    :password             => "Alzater123",
-    :authentication       => 'plain',
-    :enable_starttls_auto => true 
-  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
